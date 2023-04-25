9dac228812c74424a92ccfd913563449e3f7d37e
kabirsinghcodes <kabirsingh0405@gmail.com>
2023-04-25 20:19:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// var a = 10;
+// console.log(a);
+// var b = 10.6;
+// console.log(b);     
+// var address;
+// console.log(address);
+// var c = 1/0;
+// console.log(c);
+// var d = Math.pow(10 , 1000);
+// console.log(d);
+// var e = Number.MAX_VALUE;
+// console.log(e);
+// var f = Math.sqrt(-1);
+// console.log(f);
+// console.log(typeof a);
+// var j = null;
+// console.log(typeof j);
+
+// var a = 10;
+// if (a > 0)
+// {
+//                     console.log("positive");
+// }
+// else{
+//                     console.log("negative");
+// }
+
+// var a = 0; For Number
+// if (a)
+// {
+//                     console.log("True");
+// }
+// else{
+//                     console.log("False");
+// }
+
+// var a = "abc"; For String
+// if (a)
+// {
+//                     console.log("True");
+// }
+// else{
+//                     console.log("False");
+// }
+
+// LOOPS
+
+// var i = 1;
+// while(i <= 5)
+// {
+//                     console.log(i);
+//                     i++;
+// }
+// for( j = 1 ; j <= 5 ; j++)
+// {
+//                     console.log(j);
+// }
+// do{
+//                     console.log(i);
+//                     i++;
+// }while(i <= 5)
